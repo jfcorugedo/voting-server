10189e024292bd2685790824bb9da3912d19eaa5
jfcorugedo <jfcorugedo@gmail.com>
2016-04-27 12:30:22 +0200

    implements core logic

--- src/core.js
+++ src/core.js
@@ -1,5 +1,65 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
+/**
+ * Initializes entries of this state. It accepts any iterable  object but it always stores it as immutable
+ * @param state Current state
+ * @param entries Iterable object with all the entries that should be added to the state
+ * @returns {*} next state with all the changes applied
+ */
 export function setEntries(state, entries) {
     return state.set('entries', List(entries));
+}
+
+/**
+ * Takes the next pair of elements to vote for
+ * @param state Current state
+ * @returns {*} next state with all the changes applied
+ */
+export function next(state) {
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
+
+    if(entries.size == 1) {
+        return state.remove('vote')
+                    .remove('entries')
+                    .set('winner', entries.first());
+    } else {
+        return state.merge({
+            vote: Map({pair: entries.take(2)}),
+            entries: entries.skip(2)
+        });
+    }
+}
+
+/**
+ * Returns the winner of the current voting phase.
+ *
+ * If the voting session is tied both elements will be returned
+ * @param vote
+ */
+function getWinners(vote) {
+    if(!vote) return [];
+
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+
+    if(aVotes > bVotes) {
+        return [a];
+    } else if(aVotes < bVotes) {
+        return [b];
+    } else {
+        return [a, b];
+    }
+}
+
+/**
+ * Updates the tally of the given entry due to a new vote has been received for it
+ * @param state
+ * @param entry
+ * @returns {*} next state with all the changes applied
+ */
+export function vote(state, entry) {
+    //updateIn performs an operation over the given element (vote > tally > entry). If that element does not exists
+    //it will create it with the default value specified (0 in this case)
+    return state.updateIn(['vote', 'tally', entry], 0, tally => tally + 1);
 }
--- test/coreSpec.js
+++ test/coreSpec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -27,6 +27,146 @@ describe('application logic', () => {
             expect(nextState).to.equal(Map({
                 entries: List.of('Avengers', 'Antman')
             }));
+        });
+    });
+
+    describe('next', () => {
+
+        it('takes the next two entries under vote', () => {
+            const state = Map(
+                {
+                    entries: List.of('Avengers', 'Antman', 'Superman')
+                });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map(
+                {
+                    vote: Map({
+                        pair: List.of('Avengers', 'Antman')
+                    }),
+                    entries: List.of('Superman')
+                }
+            ));
+        });
+
+        it('puts winner of the current vote back to entries', () => {
+
+            const state = Map({
+                vote: Map(
+                    {
+                        pair:  List.of('Avengers', 'Antman'),
+                        tally: Map({'Avengers' : 3, 'Antman' : 2})
+                    }),
+                entries: List.of('Superman')
+            });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                vote: Map(
+                    {
+                        pair:  List.of('Superman', 'Avengers')
+                    }),
+                entries: List()
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+
+            const state = Map({
+                vote: Map(
+                    {
+                        pair:  List.of('Avengers', 'Antman'),
+                        tally: Map({'Avengers' : 3, 'Antman' : 3})
+                    }),
+                entries: List.of('Superman')
+            });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                vote: Map(
+                    {
+                        pair:  List.of('Superman', 'Avengers')
+                    }),
+                entries: List.of('Antman')
+            }));
+        });
+
+        it('sets the winner when there is only one entry left', () => {
+
+            const state = Map(
+                {
+                    vote: Map(
+                        {
+                            pair: List.of('Avengers', 'Antman'),
+                            tally: Map(
+                                {
+                                    Avengers: 5,
+                                    Antman: 1
+                                }
+                            )
+                        }),
+                    entries: List()
+                }
+            );
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map(
+                {
+                    winner: 'Avengers'
+                }
+            ));
+        });
+    });
+
+    describe('vote', () => {
+
+        it('creates a tally for the vote entry', () => {
+            const state = Map({
+                vote: Map({pair: List.of('Avengers', 'Antman')}),
+                entries: List.of('Superman')
+            });
+
+            const nextState = vote(state, 'Avengers');
+
+            expect(nextState).to.equal(Map(
+                {
+                    vote: Map(
+                        {
+                            pair: List.of('Avengers', 'Antman'),
+                            tally: Map({'Avengers': 1})
+                        }),
+                    entries: List.of('Superman')
+                }
+            ));
+        });
+
+        it('increment tally of the given entry', () => {
+            const state = Map({
+                vote: Map(
+                    {
+                        pair:  List.of('Avengers', 'Antman'),
+                        tally: Map({'Avengers' : 1, 'Antman' : 2})
+                    }),
+                entries: List.of('Superman')
+            });
+
+            const nextState = vote(state, 'Avengers');
+
+            expect(nextState).to.equal(Map(
+                {
+                    vote: Map(
+                        {
+                            pair:  List.of('Avengers', 'Antman'),
+                            tally: Map({'Avengers' : 2, 'Antman' : 2})
+                        }
+                    ),
+                    entries: List.of('Superman')
+                }
+            ));
         });
     });
 });
