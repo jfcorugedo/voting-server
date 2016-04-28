ce8e19d8248c27448056ae88f1181107dbd2e937
jfcorugedo <jfcorugedo@gmail.com>
2016-04-28 12:31:57 +0200

    initializes store with some hero movies during application startup

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import startServer from './src/server'
 export const store = makeStore();
 startServer(store);
 
+console.log('Initializing store....');
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./config/entries.json')
+});
+store.dispatch({type: 'NEXT'});
+
+console.log('Store initialized:\n', store.getState().toJS());
+
