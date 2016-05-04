07d2b22bf84323621036730fe31f890f846e637b
jfcorugedo <jfcorugedo@gmail.com>
2016-05-04 17:08:32 +0200

    improves documentation

--- src/server.js
+++ src/server.js
@@ -16,7 +16,9 @@ export default function startServer(store) {
     );
 
     io.on('connection', socket => {
+        //Send current state to any new client
         socket.emit('state', store.getState().toJS());
+        //When new data is received it will be sent to the store to be processed
         socket.on('action', (data) => {store.dispach(data)});
     });
 }
